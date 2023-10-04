045556b82b27152272e4317a13d7d7f6d20fb755
hanswatkins <hanswatkins@gmail.com>
2023-10-03 21:12:05 -0700

    fixed width issues on contactpage.jsx

--- src/routes/contactNew.jsx
+++ src/routes/contactNew.jsx
@@ -1,28 +1,28 @@
-import React from "react";
-import ContactForm from "./contactform";
+import React from 'react';
+import ContactForm from './contactform';
 const contactNew = () => {
-  return (
-    <div className="-mt-22 md:-mt-36 lg:-mt-46">
-      <div className="bg-kurt h-screen z-0 bg-cover bg-center bg-no-repeat"></div>
-      <div className="flex flex-col m-6 sm:m-16 md:m-14 md:grid md:grid-cols-2 md:items-center min-h-[400px] whitespace-pre-wrap gap-5">
-        <div className="mt-24 md:mt-0 font-header w-2/3">
-          <h1 className="text-3xl md:text-5xl font-bold ">Kurt Watkins</h1>
-          <h1 className="text-3xl text-gray-500 font-bold md:text-4xl">
-            SportReps NW
-          </h1>
-        </div>
-        <p className="mt-5 md:mt-0 font-body text-lg">
-          Kurt is an enthusiastic sales professional with 35 years of experience
-          as a sales representative in the sports industry. His in-depth
-          knowledge of his customer's needs allows him to exceed customer
-          expectations and provide excellent service. He is passionate about
-          developing and maintaining longstanding relationships centered on
-          trust with each of his customers.
-        </p>
-      </div>
-      <ContactForm />
-    </div>
-  );
+	return (
+		<div className='-mt-22 md:-mt-36 lg:-mt-46'>
+			<div className='bg-kurt h-1/2-screen z-0 bg-cover bg-center bg-no-repeat'></div>
+			<div className='flex flex-col m-6 sm:m-16 md:m-14 md:grid md:grid-cols-2 md:items-center min-h-[400px] whitespace-pre-wrap gap-5'>
+				<div className='mt-24 md:mt-0 font-header w-2/3'>
+					<h1 className='text-3xl md:text-5xl font-bold '>Kurt Watkins</h1>
+					<h1 className='text-3xl text-gray-500 font-bold md:text-4xl'>
+						SportReps NW
+					</h1>
+				</div>
+				<p className='mt-5 md:mt-0 font-body text-lg'>
+					Kurt is an enthusiastic sales professional with 35 years of experience
+					as a sales representative in the sports industry. His in-depth
+					knowledge of his customer's needs allows him to exceed customer
+					expectations and provide excellent service. He is passionate about
+					developing and maintaining longstanding relationships centered on
+					trust with each of his customers.
+				</p>
+			</div>
+			<ContactForm />
+		</div>
+	);
 };
 
 export default contactNew;
--- src/routes/contactform.jsx
+++ src/routes/contactform.jsx
@@ -1,59 +1,60 @@
-import React from "react";
+import React from 'react';
 
 const ContactForm = () => {
-  return (
-    <div
-      id="contact"
-      className="flex justify-center items-center text-center mx-auto bg-white bg-boreal h-1/2 bg-no-repeat "
-    >
-      <form
-        autocomplete="off"
-        name="contact"
-        method="post"
-        action="/success"
-        className="flex flex-col gap-4 w-full m-8"
-      >
-        <h1 className="text-white text-3xl">Contact Me</h1>
-        <input class="input" type="hidden" name="form-name" value="contact" />
+	return (
+		<div
+			id='contact'
+			className='flex justify-center items-center text-center mx-auto bg-white bg-boreal h-1/2 bg-cover '
+		>
+			<form
+				autocomplete='off'
+				name='contact'
+				method='post'
+				action='/success'
+				className='flex flex-col gap-4 w-full md:w-1/2
+         m-8'
+			>
+				<h1 className='text-white text-3xl'>Contact Me</h1>
+				<input class='input' type='hidden' name='form-name' value='contact' />
 
-        <label htmlFor="name" className="[&>*]:w-3/4 lg:[&>*]:w-1/2">
-          <input
-            placeholder="Name"
-            className="p-2 py-1  focus:outline-gray-600"
-            type="text"
-            name="name"
-            required
-          />
-        </label>
+				<label htmlFor='name' className='[&>*]:w-3/4 lg:[&>*]:w-1/2'>
+					<input
+						placeholder='Name'
+						className='p-2 py-1  focus:outline-gray-600'
+						type='text'
+						name='name'
+						required
+					/>
+				</label>
 
-        <label htmlFor="email" className="[&>*]:w-3/4 lg:[&>*]:w-1/2">
-          <input
-            placeholder="Email"
-            className=" focus:outline-gray-600 py-1 p-2"
-            type="email"
-            name="email"
-            required
-          />
-        </label>
+				<label htmlFor='email' className='[&>*]:w-3/4 lg:[&>*]:w-1/2'>
+					<input
+						placeholder='Email'
+						className=' focus:outline-gray-600 py-1 p-2'
+						type='email'
+						name='email'
+						required
+					/>
+				</label>
 
-        <label htmlFor="message" className="[&>*]:w-3/4 lg:[&>*]:w-1/2">
-          <textarea
-            placeholder="Message"
-            className="resize-none  focus:outline-gray-600 py-1 p-2"
-            name="message"
-            required
-          ></textarea>
-        </label>
+				<label htmlFor='message' className='[&>*]:w-3/4 lg:[&>*]:w-1/2'>
+					<textarea
+						placeholder='Message'
+						className='resize-none  focus:outline-gray-600 py-1 p-2'
+						name='message'
+						required
+					></textarea>
+				</label>
 
-        <button
-          className="bg-white px-4 py-2 transition-all duration-300 hover:rounded-lg w-3/4 lg:w-1/2 mx-auto shadow-sm hover:shadow-none hover:scale-95 active:scale-75"
-          type="submit"
-        >
-          Send
-        </button>
-      </form>
-    </div>
-  );
+				<button
+					className='bg-white px-4 py-2 transition-all duration-300 hover:rounded-lg w-3/4 lg:w-1/2 mx-auto shadow-sm hover:shadow-none hover:scale-95 active:scale-75'
+					type='submit'
+				>
+					Send
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default ContactForm;
