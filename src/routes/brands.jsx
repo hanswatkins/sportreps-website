00941819cdd8e6a5709237eafa91e665502e950e
hanswatkins <hanswatkins@gmail.com>
2024-12-01 14:49:23 -0800

    fixed tahoe link on brands.jsx

--- src/routes/brands.jsx
+++ src/routes/brands.jsx
@@ -80,7 +80,7 @@ const Brands = () => {
             </h1>
           </div>
         </Link>
-        <Link to={"/taho-blue-gear"}>
+        <Link to={"/tahoe-blue-gear"}>
           <div className="relative group">
             <img src={tahohero} alt="..." />
             <h1 className="opacity-0 group-hover:opacity-100 transition-all duration-200 flex group-hover:cursor-pointer items-center justify-center text-white text-3xl font-header font-bold absolute top-0 w-full h-full bg-gradient-to-r from-white/20 to-gray-500/20">
